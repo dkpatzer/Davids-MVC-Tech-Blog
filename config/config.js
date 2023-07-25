c7a063ab92d8853f489a6508c267d98baf477618
“firstname <38668042+dkpatzer@users.noreply.github.com>
2023-07-25 16:38:19 -0500

    trying to make it all work together

--- config/config.js
+++ config/config.js
@@ -1,11 +1,10 @@
 const Sequelize = require('sequelize');
-
 require('dotenv').config();
 
 // create connection to our db
 const sequelize = process.env.JAWSDB_URL
   ? new Sequelize(process.env.JAWSDB_URL)
-  : new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
+  : new Sequelize(process.env.davids_mvc_tech_blog, process.env.root, process.env.Password1, {
       host: 'localhost',
       dialect: 'mysql',
       port: 3306
