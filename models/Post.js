9b414cada9ba698e3c5fded85c10591713895d84
“firstname <38668042+dkpatzer@users.noreply.github.com>
2023-07-25 13:58:29 -0500

    added file data

---
+++ models/Post.js
@@ -0,0 +1,39 @@
+// models/Post.js
+const { DataTypes } = require('sequelize');
+const sequelize = require('../config/database');
+
+const Post = sequelize.define('Post', {
+  id: {
+    type: DataTypes.INTEGER,
+    primaryKey: true,
+    autoIncrement: true,
+  },
+  title: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  content: {
+    type: DataTypes.TEXT,
+    allowNull: false,
+  },
+  createdAt: {
+    type: DataTypes.DATE,
+    allowNull: false,
+    defaultValue: DataTypes.NOW,
+  },
+  updatedAt: {
+    type: DataTypes.DATE,
+    allowNull: false,
+    defaultValue: DataTypes.NOW,
+  },
+  userId: {
+    type: DataTypes.INTEGER,
+    allowNull: false,
+    references: {
+      model: 'User',
+      key: 'id',
+    },
+  },
+});
+
+module.exports = Post;
---
+++ models/index.js
@@ -0,0 +1,8 @@
+// models/index.js
+const Comment = require('./Comment');
+const User = require('./User');
+const Post = require('./Post');
+
+// Define any associations or relationships between models here, if needed
+
+module.exports = { Comment, User, Post };
