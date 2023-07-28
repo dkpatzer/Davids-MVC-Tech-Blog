b381614d4a8ebdb0d24ccf06e80d7cf18524acbb
“firstname <38668042+dkpatzer@users.noreply.github.com>
2023-07-27 22:37:08 -0500

    reverted connect.js

--- config/connection.js
+++ config/connection.js
@@ -1,10 +1,7 @@
-
-// Import the Sequelize constructor from the library
+require('dotenv').config();
 const Sequelize = require('sequelize');
 
-require('dotenv').config();
 
-// Create connection to our database, pass in your MySQL information for username and password
 let sequelize;
 
 if (process.env.JAWSDB_URL) {
@@ -12,11 +9,12 @@ if (process.env.JAWSDB_URL) {
 } else {
   sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PW, {
     host: 'localhost',
-    dialect: 'mysql',
-    port: 3306
+    dialect: 'mysql', // Specify the dialect here as a string
+    port: 3306 // Set the port to 3306 for MySQL
   });
 }
 
 module.exports = sequelize;
+
 
 
